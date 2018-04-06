e6fe9993937c5923631cb0b85ca8b9942c9ec032
filipwo <38138515+filipwo@users.noreply.github.com>
2018-04-06 18:47:35 +0200

    w
    xd

--- realtimeprocessing/src/main/java/Save.java
+++ realtimeprocessing/src/main/java/Save.java
@@ -2,13 +2,8 @@ import twitter4j.*;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
-import java.util.Date;
-
-public class TwitterStreamer {
-    Status[] top10 = new Status[11];
-
-
-    public void stream(final RealTimeGUI gui){
+public class Save {
+    public static void main(String[] args) {
         final Configuration configuration = new ConfigurationBuilder()
                 .setDebugEnabled(true)
                 .setOAuthConsumerKey("cHkOtFiELcZ7nwloE5Nfq1iDs") // paste your consumer key
@@ -16,19 +11,14 @@ public class TwitterStreamer {
                 .setOAuthAccessToken("981949789192507393-Li90Uw21NiPVWKc5HeK8A89ENTe7fzh") //paste your OAUTH_ACCESS_TOKEN
                 .setOAuthAccessTokenSecret("Pwe13QzbzbzIjAM3p6TjoXrJwEUbi0vIM5zs6Xc58GzEo") // paste your OAUTH_ACCESS_TOKEN
                 .build();
-
         StatusListener listener = new StatusListener() {
 
             public void onStatus(Status status) {
-                Date time = status.getCreatedAt();
-                User user = status.getUser();
-                String name = user.getName();
-                int followers = user.getFollowersCount();
-                Place place = status.getPlace();
-                String country = place.getCountry();
 
-                gui.displayTweet(status);
-                //System.out.println(status.getText());
+                System.out.println(status.getText());
+
+
+
             }
 
             public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
@@ -52,6 +42,8 @@ public class TwitterStreamer {
 
         TwitterStream twitterStream = new TwitterStreamFactory(configuration).getInstance();
         twitterStream.addListener(listener);
-        twitterStream.filter(new FilterQuery().track("Donald"));
+        twitterStream.filter(new FilterQuery().track("DUDA", "UK", "USA"));
+
+
     }
 }
